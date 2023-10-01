cfbf6dd747f1c6e9de0afe792e9c67b4775cae00
jamikrut <jamikrut@student.wszib.edu.pl>
2023-10-01 15:39:59 +0200

    all_collection - ammend

---
+++ movie/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+
+class MovieForm(forms.Form):
+    tmdb_id = forms.CharField(max_length=15, required=False, label='TMDB ID')
+    title = forms.CharField(max_length=255, label='Tytuł')
+    cast = forms.CharField(max_length=255, label='Obsada')
+    homepage = forms.URLField(label='Strona WWW')
+    director = forms.CharField(max_length=255, label='Reżyser')
+    keywords = forms.CharField(max_length=255, label='Słowa kluczowe')
+    overview = forms.CharField(max_length=1000, label='Opis', widget=forms.Textarea(attrs={'rows': 3}))
+    runtime_min = forms.IntegerField(label='Czas trwania [minuty]')
+    genres = forms.CharField(max_length=255, label='Gatunki [rozdzielone pipe]')
+    production_companies = forms.CharField(max_length=1000, label='Producenci [rozdzielone pipe]')
+    release_date = forms.DateField(label='Data produkcji', widget=forms.DateInput(attrs={"type": "date"}))
--- movie/views.py
+++ movie/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponseRedirect
 from django.db.models import Count
 
 # Create your views here.
 from . import models
+from .forms import MovieForm
 
 
 def selected_movie(request, title):
@@ -30,6 +31,21 @@ def movie_details(request, id):
     found_movie = models.Movie.objects.get(pk=id)
     return render(request, 'movie/movie_details.html', {
         'movie': found_movie
+    })
+
+
+def add_movie(request):
+    if request.method == 'POST':
+        print(request.POST.get('title'))
+        print(request.POST.get('cast'))
+        print(request.POST.get('date'))
+        print(request.body)
+
+        return redirect('all_movies')
+
+    movie_form = MovieForm()
+    return render(request, 'movie/add_movie.html', {
+        'movie_form': movie_form
     })
 
 
